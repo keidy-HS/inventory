cd7064faaa247a11b92c264e7c6d31db6b6591dc
keidy-HS <71207695+keidy-HS@users.noreply.github.com>
2020-09-24 17:20:31 +0000

    Add files via upload

--- resources/views/items/edit.blade.php
+++ resources/views/items/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="content-wrapper col-md-12">
+<div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <div class="container-fluid">
@@ -24,55 +24,56 @@
       <div class="container-fluid">
         <div class="row">
           <!-- left column -->
-          <div class="col-md-12">
+          <div class="col-md-6">
             <!-- general form elements -->
             <div class="card card-primary">
               <div class="card-header">
                 <h3 class="card-title">Descriptions</h3>
               </div>
               <!-- /.card-header -->
+
               <!-- form start -->
-              <form role="form">
+              <form action="{!! route('item.store') !!}" method="POST" role="form">
+                @csrf
                 <div class="card-body">
                   <div class="form-group">
                     <label for="exampleInputEmail1">Nom ***</label>
-                    <input type="text" name="name" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                  </div>
+                    <input type="text" name="name" class="form-control" id="exampleInputEmail1" value="{{old('name') ?? $categories->nom}}"placeholder="Nom du produit" required>
+                  </div>{{-- 
                   <div class="form-group">
                     <label for="exampleInputPassword1">Code produit ***</label>
-                    <input type="text" name="code" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                  </div>
+                    <input type="text" name="code" class="form-control" id="exampleInputPassword1" placeholder="Password" required>
+                  </div> --}}
                   <div class="form-group">
                     <label for="exampleInputPassword1">Prix d'achat ***</label>
-                    <input type="text" name="" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    <input type="text" name="purchase_price" class="form-control" id="exampleInputPassword1" value="{{old('purchase_price') ?? $categories->nom}}" placeholder="Prix d'achat" required>
                   </div>
                   <div class="form-group">
                     <label for="exampleInputPassword1">Prix de vente ***</label>
-                    <input type="text" name="" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    <input type="text" name="retail_price" class="form-control" id="exampleInputPassword1"  value="{{old('retail_price') ?? $categories->nom}}" placeholder="Prix de vente" required>
                   </div>
                   <div class="form-group">
                     <label for="exampleInputPassword1">TVA</label>
-                    <input type="text" name="" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    <input type="text" name="vat" class="form-control" id="exampleInputPassword1"  value="{{old('vat') ?? $categories->vat}}" placeholder="TAX: TVA">
                   </div>
                   <div class="form-group">
                         <label>Category</label>
-                        <select class="form-control">
-                          <option>option 1</option>
-                          <option>option 2</option>
-                          <option>option 3</option>
-                          <option>option 4</option>
-                          <option>option 5</option>
+                        <select class="form-control" name="category">
+                            <option value="{{old('category') ?? $categories->categories}}">Categorie Produit</option>
+                          @foreach($categories as $category)
+                            <option value="{!! $category->slug !!}">{!! $category->name !!}</option>
+                          @endforeach
                         </select>
                       </div>
                   <div class="form-group">
                     <label for="exampleInputPassword1">Quantité ***</label>
-                    <input type="text" name="" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    <input type="text" name="quantity" class="form-control" id="exampleInputPassword1" value="{{old('quantity') ?? $categories->quantity}}" placeholder="Quantité à Ajouter" required>
                   </div>
                   <div class="form-group">
-                    <label for="exampleInputPassword1">Prix d'achat ***</label>
-                    <input type="text" name="" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                  </div>
-                  <div class="form-group">
+                        <label>Description</label>
+                        <textarea name="description" class="form-control" rows="3" value="{{old('description') ?? $categories->description}}"placeholder="Description du produit ..."></textarea>
+                    </div>
+                  {{-- <div class="form-group">
                     <label for="exampleInputFile">Photo</label>
                     <div class="input-group">
                       <div class="custom-file">
@@ -80,7 +81,7 @@
                         <label class="custom-file-label" for="exampleInputFile">Choisissez l'image</label>
                       </div>
                     </div>
-                  </div>
+                  </div> --}}
                 </div>
                 <!-- /.card-body -->
 
@@ -88,6 +89,9 @@
                   <button type="submit" class="btn btn-primary">Ajouter</button>
                 </div>
               </form>
+              <!-- Form End -->
+
+
             </div>
               </div>
               <!-- /.card-body -->
@@ -97,4 +101,5 @@
           <!--/.col (right) -->
         </section>
     <!-- /.content -->
+</div>
 @endsection
